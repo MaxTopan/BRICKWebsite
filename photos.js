2489d275d0ef25e1c371ce05ab5518c437ac4eeb
Max Topan <maxtopan@hotmail.co.uk>
2025-09-17 22:35:06 +0100

    reverse order of photo appearances

--- photos.js
+++ photos.js
@@ -1,15 +1,9 @@
-/* <div class="gallery">
-    <a target="_blank" href="assets/SigilVectorWhite-cropped.svg">
-      <img src="assets/SigilVectorWhite-cropped.svg" alt="Cinque Terre" width="600" height="400">
-    </a>
-  </div> */
-
 fetch("assets/photos.json")
   .then(res => res.json())
   .then(files => {
     const photosContainer = document.getElementById("photos-container");
+    files.reverse();
     files.forEach(file => {
-      //const img = document.createElement("img");
       const imgsrc = "assets/photos/" + file;
       photosContainer.innerHTML += `
       <div class="gallery responsive">
@@ -17,7 +11,5 @@ fetch("assets/photos.json")
         <img src="${imgsrc}" >
         </a>
       </div>`
-      // photosContainer.appendChild(img);
-      console.log(file);
     });
   });
