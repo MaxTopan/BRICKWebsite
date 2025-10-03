8b891d3c9bdf1216af31563d32ae2e01fa43f166
Max Topan <maxtopan@hotmail.co.uk>
2025-10-03 09:01:05 +0100

    convert html files to njk files

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     eleventyConfig.setTemplateFormats(["html", "njk", "txt", "js", "css", "xml", "json"]);
     eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
-    eleventyConfig.addPassthroughCopy("_src/syle.css")
+    eleventyConfig.addPassthroughCopy("_src/syle.css");
+    eleventyConfig.addPassthroughCopy("_src/*.css");
+    eleventyConfig.addPassthroughCopy("_src/index.html");
+    eleventyConfig.addPassthroughCopy("_src/assets");
+    eleventyConfig.addFilter("toBrickCase", function (value) {
+        if (!value) return "";
+        return `[${value.toUpperCase()}]`;
+    })
 
     return {
         dir: {
